f91a2267c1ce7c51c8508787671331ad65317dfe
ethiotom <temesgenwondim77@gmail.com>
2025-06-21 11:00:16 +0300

    this is the first commit

---
+++ ifStatment.py
@@ -0,0 +1,11 @@
+num = input ("enter the number")
+num = int(num)
+if num%2 == 0:
+    print('number is even'+ str(num))
+else:
+    print ('number is odd',num)
+
+
+for statatment
+
+
---
+++ test.py
@@ -0,0 +1,2 @@
+# print("hello")
+# print('this is the starting point to learn python for AI engineering')
