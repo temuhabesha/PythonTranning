0bfd9b9152fd9b7cd8316ce5534ca43b0da219d5
ethiotom <temesgenwondim77@gmail.com>
2025-06-21 11:35:26 +0300

    this is the commit when i see loop in python

---
+++ loopStatment.py
@@ -0,0 +1,24 @@
+exp = [2,4,6,4,2]
+
+# total = 0
+# for item in exp:
+#     total = total + item
+# print (total)
+
+# # to list the data in the range
+# for item in range(0,10):
+#  print(item)
+
+# # other example for range method 
+# for i in range(len(exp)):
+#     print(exp[i])
+
+
+# # when we use continue
+# for i in range(1,10):
+#     if i%2 == 0:
+#         continue
+#     else:
+#         print('the number is', i)
+
+        
