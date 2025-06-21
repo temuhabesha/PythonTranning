186c986f6469622cff35910aa7ebae91c1b8a84c
ethiotom <temesgenwondim77@gmail.com>
2025-06-21 12:09:15 +0300

    this is the commit when i see function in python

---
+++ function.py
@@ -0,0 +1,19 @@
+# def any_sum(results):
+#     sum = 0
+#     for result in results:
+#         sum = sum + result
+#     return sum
+# tom = [1,2,3,4,5]
+# selam = [6,7,8,9,0]
+
+# totaltom = any_sum(tom)
+# totalselam = any_sum(selam)
+
+# print ('total result for tom is', totaltom)
+# print ('total result for selli is', totalselam)
+
+# # the other example of function which returns the sum of two numbers
+# def sum(a,b):
+#     total = 0
+#     return a+b
+# print(sum(3,1))
