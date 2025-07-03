5b2eacbf517b04c77b1ec21c074870c6213809c0
ethiotom <temesgenwondim77@gmail.com>
2025-07-03 05:05:41 +0300

    the commit when i train somethin

--- function.py
+++ function.py
@@ -12,8 +12,32 @@
 # print ('total result for tom is', totaltom)
 # print ('total result for selli is', totalselam)
 
-# # the other example of function which returns the sum of two numbers
+# the other example of function which returns the sum of two numbers
 # def sum(a,b):
+#     """this function takes two integer numbers and it returns the some of that two numbers this is documentation in python"""
 #     total = 0
 #     return a+b
 # print(sum(3,1))
+
+## module in python
+
+import math
+# result = math.sqrt(16)
+# print(result)
+# print(dir(math))
+# print(math.log10(100))
+
+import calendar
+
+# print(calendar.month(2016,1))
+# print(calendar.isleap(2016))
+# print(dir(calendar))
+
+# how we write a custom module in python
+# import modultest
+
+# print(modultest.sum(4,2))
+
+# working with JSON in pyhon 
+# book = {}
+# book['tom'] = {'name':'tom',}
